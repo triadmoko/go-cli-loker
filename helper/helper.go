154840392b327733af2aee2db11f03d50a4710ef
triadmoko <triadmoko12@gmail.com>
2022-04-07 01:20:20 +0700

    input

--- helper/helper.go
+++ helper/helper.go
@@ -1,1 +1,26 @@
 package helper
+
+import "fmt"
+
+func SplitArgument(input []string) (command string, arg1 string, arg2 string) {
+	if len(input) == 0 { //cek total input, jika nol ulangi
+		return
+	}
+
+	command = input[0]
+
+	if len(input) > 2 {
+		arg1, arg2 = input[1], input[2]
+	} else if len(input) > 1 {
+		arg1 = input[1]
+	}
+	return
+}
+
+func Result(res string, err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println(res)
+	}
+}
--- input/input.go
+++ input/input.go
@@ -1,1 +1,20 @@
-package input
+package entity
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func Input() []string {
+	newReader := bufio.NewReader(os.Stdin)   // Mengambil input
+	input, err := newReader.ReadString('\n') // mengambil 1 field dari input
+	// cek error
+	if err != nil {
+		fmt.Println(errors.New("inputan gagal"))
+	}
+	inputFields := strings.Fields(input) // menjadikan input ke dalam slice
+	return inputFields                   // mengembalikan data
+}
